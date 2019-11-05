3e1918f7cf4668512319c93fcdce262c1961b8da
TychoNeuman <tycho.neuman@mijngeldzaken.nl>
2019-11-05 19:47:01 +0100

    working on blink

--- GameOff2019/Assets/_Scripts/PlayerMovement.cs
+++ GameOff2019/Assets/_Scripts/PlayerMovement.cs
@@ -8,27 +8,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float speed;
+    private double blinkTime;
     private Rigidbody2D playerRigidBody;
 
     void Start()
     {
         playerRigidBody = this.GetComponent<Rigidbody2D> ();
         speed = 10;
+        blinkTime = 0.2;
     }
 
     void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
         Vector3 tempVect = new Vector3(h, v, 0);
         tempVect = tempVect.normalized * speed * Time.deltaTime;
         playerRigidBody.MovePosition(playerRigidBody.transform.position + tempVect);
 
         if(Input.GetKeyDown(KeyCode.Space)){
-            playerRigidBody.MovePosition(playerRigidBody.transform.position + tempVect * 12);
+            // playerRigidBody.MovePosition(playerRigidBody.transform.position + tempVect * 12);
+            StartCoroutine(Blink());
+            StopCoroutine(Blink());
         }
+
+        if(blinkTime <= 0) {
+            speed = 10;
+            blinkTime = 0.2;
+        }
+
     }
 
+    IEnumerator Blink ()
+    {
+        while(blinkTime > 0){
+            blinkTime -= Time.deltaTime;
+            speed = 40;
+            yield return null;
+        }
+    }
 
 }
