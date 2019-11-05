47fd0cb7b5bfde98780adb331a2296006c62d0dd
TychoNeuman <tycho.neuman@mijngeldzaken.nl>
2019-11-05 18:47:59 +0100

    initial player movement test

---
+++ GameOff2019/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed;
+    private Rigidbody2D playerRigidBody;
+
+    void Start()
+    {
+        playerRigidBody = this.GetComponent<Rigidbody2D> ();
+        speed = 10;
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        playerRigidBody.MovePosition(playerRigidBody.transform.position + tempVect);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            playerRigidBody.MovePosition(playerRigidBody.transform.position + tempVect * 12);
+        }
+    }
+
+
+}
